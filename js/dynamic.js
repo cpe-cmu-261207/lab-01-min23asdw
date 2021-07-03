5ddf15bcd5174d65486ccab6a7679a9d2d300e76
min23asdw <min23asdw@outlook.com>
2021-07-03 19:05:57 +0700

    RM js

--- js/dynamic.js
+++
@@ -1,8 +0,0 @@
-scrollingElement = (document.scrollingElement || document.body)
-
-//Require jQuery
-function scrollSmoothToBottom (id) {
-   $(scrollingElement).animate({
-      scrollTop: document.body.scrollHeight
-   }, 500);
-}
